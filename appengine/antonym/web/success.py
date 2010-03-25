232f515204472a2e9c35dabd16427bf285e832c5
matthew <mhawthorne@gmail.com>
2010-03-25 17:12:52 -0400

    endpoint for following, unfollowing, viewing followers with specified friend-to-follower ratio.  added generic http task to rakefile to I can hit endpoints without writing mini service clients in ruby.

---
+++ appengine/antonym/web/success.py
@@ -0,0 +1,26 @@
+from google.appengine.ext import webapp
+from google.appengine.ext.webapp.util import run_wsgi_app
+
+from katapult.requests import RequestHelper
+
+# hack attempt to make a deferred task finish
+
+class SuccessHandler(webapp.RequestHandler):
+    
+    def get(self):
+        self.__default()
+        
+    def post(self):
+        self.__default()
+
+    def __default(self):
+        helper = RequestHelper(self)
+        helper.write_json({'msg': 'hi'})
+
+application = webapp.WSGIApplication([('/.*', SuccessHandler)])
+
+def main():
+    run_wsgi_app(application)
+
+if __name__ == "__main__":
+    main()
