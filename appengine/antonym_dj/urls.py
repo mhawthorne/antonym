bc68dc8fc5c83ee20e8d8925693eba896f13a925
matthew hawthorne <mhawthorne@gmail.com>
2011-09-15 22:22:59 -0700

    updating shell

--- appengine/antonym_dj/urls.py
+++ appengine/antonym_dj/urls.py
@@ -5,5 +5,6 @@ prefix = "-/"
 urlpatterns = patterns('',
     (r"^%sspeak/?(.+)?$" % prefix, "antonym_dj.root.views.speak"),
     (r"^%ssearch$" % prefix, "antonym_dj.root.views.search"),
-    (r"^%sshell$" % prefix, "antonym_dj.root.views.shell")
+    (r"^%sshell$" % prefix, "antonym_dj.root.views.shell"),
+    (r"^$", "antonym_dj.root.views.shell")
 )
